753906d9b51fbb9cd5f604e38c65d901c9281802
sun10107 <sun10107@mail.ru>
2022-01-22 22:49:38 +0300

    new changes

--- src/overone/com/lessons/les_4/Task.java
+++ src/overone/com/lessons/les_4/Task.java
@@ -1,7 +1,16 @@
 package overone.com.lessons.les_4;
 
+import java.util.Scanner;
+
 public class Task {
     public static void main(String[] args) {
-        //
+        int i=1;
+        int j;
+        for( j=0; j<25;j=j+3){
+           if (j<25);
+           i=i*2;
+               {
+        System.out.println(i);
+
     }
-}
+    }}}
--- ye.java
+++ ye.java
@@ -3,7 +3,7 @@ public class ye
     int ameba=3;
     int time=0
 
-    for( int ameba=1; time=0;ameba=1*2; time<=24;time +2;ameba++)
-}
+    for( int ameba=1; ameba=<2;ameba++)
+        }
 System.out.printeln( ameba)}
         }
