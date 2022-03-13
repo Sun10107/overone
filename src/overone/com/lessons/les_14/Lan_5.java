dc26ebd430fe1a0461a2f41ee37e1593d68a1556
User <sun10107@mail.ru>
2022-03-13 10:13:10 +0300

    initial

--- src/overone/com/lessons/les_14/Lan_5.java
+++ src/overone/com/lessons/les_14/Lan_5.java
@@ -1,6 +1,5 @@
 package overone.com.lessons.les_14;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lan_5 {
@@ -8,6 +7,6 @@ public class Lan_5 {
           public static void main(String[] arge) {
               System.out.println("Enter a number or wolds");
               Scanner scanner = new Scanner(System.in);
-              int[] arrays = new int[];
+
               int number = scanner.nextInt();
           }         }
